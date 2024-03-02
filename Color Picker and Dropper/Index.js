b0ee72f9b6fbb4fad5678c098a847c5b63757a7a
Chand Shaikh <chandshaikh7778@gmail.com>
2024-03-03 01:31:13 +0530

    complete project

--- Color Picker and Dropper/Index.js
+++ Color Picker and Dropper/Index.js
@@ -1,20 +1,19 @@
-let color = document.querySelectorAll('.color')
-let empty = document.querySelectorAll('.empty')
+let color = document.querySelectorAll(".color");
+let empty = document.querySelectorAll(".empty");
 let reset = document.getElementById('reset')
 
-color.forEach((div)=>{
-  div.addEventListener('click',()=>{
-let value = div.getAttribute('data-value')
+color.forEach((div) => {
+  div.addEventListener("click", () => {
+    let value = div.getAttribute("data-value");
 
-empty.forEach((div)=>{
-  div.addEventListener('click',()=>{
-  div.style.backgroundColor = value
-  reset.addEventListener('click',()=>{
-    div.style.backgroundColor = 'white'
-  })
-})
-}
-)})
-})
+    empty.forEach((div) => {
+      div.addEventListener("click", () => {
+        div.style.backgroundColor = value;
 
-
+        reset.addEventListener('click',()=>{
+            div.style.backgroundColor = 'white'
+        })
+      });
+    });
+  });
+});
