98bb501e1ad680658779b38afc98898678e4331c
Chand Shaikh <chandshaikh7778@gmail.com>
2024-03-03 01:14:39 +0530

    updated program

--- Color Picker and Dropper/Index.js
+++ Color Picker and Dropper/Index.js
@@ -1,18 +1,38 @@
-let color = document.getElementById("color")
-let empty = document.querySelectorAll(".empty")
-let stop = document.getElementById('stop')
+// let color = document.getElementById("color")
+// let empty = document.querySelectorAll(".empty")
+// let stop = document.getElementById('stop')
 
-function reset(){
-  empty.forEach((e)=>{
-    e.style.backgroundColor = 'white'
-  })
-}
-color.addEventListener("click",()=>{
-  let value = color.getAttribute('data-value')
-  empty.forEach((e)=>{
-    e.addEventListener('click',()=>{
-      e.style.backgroundColor = value
-      stop.addEventListener('click',reset)
-    })
-  })
+// function reset(){
+//   empty.forEach((e)=>{
+//     e.style.backgroundColor = 'white'
+//   })
+// }
+// color.addEventListener("click",()=>{
+//   let value = color.getAttribute('data-value')
+//   empty.forEach((e)=>{
+//     e.addEventListener('click',()=>{
+//       e.style.backgroundColor = value
+//       stop.addEventListener('click',reset)
+//     })
+//   })
+// })
+
+
+
+
+let color = document.querySelectorAll('.color')
+let empty = document.querySelectorAll('.empty')
+
+color.forEach((div)=>{
+  div.addEventListener('click',()=>{
+let value = div.getAttribute('data-value')
+
+empty.forEach((div)=>{
+  div.addEventListener('click',()=>{
+  div.style.backgroundColor = value
 })
+}
+)})
+})
+
+
